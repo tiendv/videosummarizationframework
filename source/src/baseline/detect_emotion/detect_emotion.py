20bff5e40a0c4eca4d4844fbc26d1223bfcd0ce7
dungmn <minhdung2060@gmail.com>
2020-04-30 15:48:08 +0700

    update detect emotion

---
+++ source/src/baseline/detect_emotion/detect_emotion.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+import os,sys,glob
+import argparse
+sys.path.append("../../../config")
+sys.path.append("../../../../libs/detect_emotion/")
+from config import cfg
+from load_model_and_processing import detect_emotion
+
+def main():
+    #************************************************************************
+    # Purpose: detect emotion from shots of a bbc video
+    # Inputs:
+    # - vid_id: id of the bbc video
+    # Output: the result csv file stored in path_emotions
+    # Author: Trivlm
+    #************************************************************************
+
+    parser = argparse.ArgumentParser(description='Optional description "BBC start = 0 & end = 243"')
+    parser.add_argument('start', type=int, help='ID of start video')
+    parser.add_argument('end', type=int, help='ID of end video')
+    args = parser.parse_args()
+
+    list_name_video = []
+    for i in range(args.start,args.end +1):
+        name_video = "video"+str(i)
+        list_name_video.append(name_video)
+    detect_emotion(PATH_FACES_SHOT_BBC,PATH_KF_SHOT_BBC,PATH_EMOTION_SHOT_BBC,list_name_video)
+
+if __name__ == '__main__':
+    # CUDA_VISIBLE_DEVICES=1 python3 detect_emotion.py 0 243
+    main()
